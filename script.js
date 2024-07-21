353d24efc4a149972ae5258ebc605137cf32c7ef
YuliyaBobrykgit <yuliyabobryk.work@gmail.com>
2024-07-21 23:13:25 +0200

    hometask-9

---
+++ script.js
@@ -0,0 +1,73 @@
+const pomodoroTime = document.getElementById('pomodoro-time');
+const startBtn = document.getElementById('start');
+const pomodoroBtn = document.getElementById('pomodoro');
+const breakBtn = document.getElementById('break');
+const resetBtn = document.getElementById('reset');
+
+function formatTimer(value) {
+    if (value < 10) {
+        return `0${value}`
+    }        
+    return value;
+}
+
+function createTimerContainer() {
+    let timerId;
+    let initialTime = pomodoroTime.textContent;
+
+    function setTimer() {
+        timerId = setInterval(() => {
+            let [mm, ss] = pomodoroTime.textContent.split(':').map(Number); // пересмотреть синтаксис и разобраться
+
+            if (ss > 0) {
+                ss--;
+            } else if (mm > 0) {
+                ss = 59;
+                mm--;
+            }
+            pomodoroTime.textContent = `${formatTimer(mm)}:${formatTimer(ss)}`;
+
+            if (mm === 0 && ss === 0) {
+                changeBtn('stop');
+                pomodoroTime.textContent = initialTime;
+            }  
+            
+        }, 1000);
+        }
+    
+    function changeBtn(startStop) {
+        if (startStop === 'stop') {
+            startBtn.textContent = 'start';
+            clearInterval(timerId);
+        } else if (startStop === 'start') {
+            startBtn.textContent = 'stop';
+            setTimer();
+        }
+    }
+
+    startBtn.addEventListener('click', function () { 
+        if (startBtn.textContent === 'start') {
+            changeBtn('start');
+        } else {
+            changeBtn('stop');
+        }   
+    }); 
+
+    pomodoroBtn.addEventListener('click', function() {
+        pomodoroTime.textContent = '00:09';
+        initialTime = pomodoroTime.textContent;
+        changeBtn('stop');
+    })
+
+    breakBtn.addEventListener('click', function() {
+        pomodoroTime.textContent = '00:05';
+        initialTime = pomodoroTime.textContent;
+        changeBtn('stop');
+    })
+
+    resetBtn.addEventListener('click', function() {
+        pomodoroTime.textContent = initialTime;
+        changeBtn('stop');
+    })
+}
+createTimerContainer();
